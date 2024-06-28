48ccb7211d56b645b41cc947aceced2bcfab90eb
Leny1050 <101010tatata1010@gmail.com>
2024-06-28 18:01:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Добавьте необходимые функции JavaScript здесь, если они будут добавлены в будущем
-
-// Пример функции для кнопки
 document.querySelector('button').addEventListener('click', function() {
     window.scrollTo({
         top: document.body.scrollHeight,
