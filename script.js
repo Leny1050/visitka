30a57d769fdf407202f3282a7bace820fefa4307
Leny1050 <101010tatata1010@gmail.com>
2024-06-28 17:58:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,10 @@
-// Показать/скрыть кнопку возврата наверх
-window.onscroll = function() {
-    var backToTopButton = document.getElementById("backToTop");
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        backToTopButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = "none";
-    }
-};
+// Добавьте необходимые функции JavaScript здесь, если они будут добавлены в будущем
 
-// Прокрутка наверх при нажатии на кнопку
-document.getElementById("backToTop").onclick = function() {
-    document.body.scrollTop = 0; 
-    document.documentElement.scrollTop = 0; 
-};
-
-// Disqus
-var disqus_config = function () {
-    this.page.url = 'https://leny1050.github.io/Flud/';  // Укажите адрес вашей страницы
-    this.page.identifier = 'https://leny1050.github.io/Flud/'; // Укажите уникальный идентификатор страницы (можете использовать URL страницы)
-};
-
-(function() { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://fludjj.disqus.com/embed.js'; // Замените на ваш короткий идентификатор
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-})();
+// Пример функции для кнопки
+document.querySelector('button').addEventListener('click', function() {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    });
+});
 
